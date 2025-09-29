81fa57e084d48e8e2cc44fc9703dffb09b37be8b
Paveljolak <paveljolak123@gmail.com>
2025-09-29 15:05:20 +0200

    Fixed the build and the www showing up.

--- docs/conf.py
+++ docs/conf.py
@@ -66,11 +66,14 @@ html_context['version'] = current_version
 
 # Get the custom domain 
 CUSTOM_DOMAIN = os.environ.get("CUSTOM_DOMAIN", "")
-k 
+
+# Get the domain with the www part
+domain_parts = CUSTOM_DOMAIN.split(".")
+VERSION_LINK = f'{REPO_NAME}.{'.'.join(domain_parts[1:])}'
 
 # If the DOMAIN exists in the workflow then use it
 if CUSTOM_DOMAIN:
-    GITHUB_PAGES_BASE = f"https://{REPO_NAME}.{CUSTOM_DOMAIN}"
+    GITHUB_PAGES_BASE = f"https://{VERSION_LINK}"
 else:
     GITHUB_PAGES_BASE = f"https://{OWNER}.github.io/{REPO_NAME}"# Set versions for selector with absolute URLs
     
