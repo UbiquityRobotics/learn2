8bb5ef67b953cc37d155c2efd4f859a617f81c19
Paveljolak <paveljolak123@gmail.com>
2026-02-02 13:03:06 +0100

    Fixed the problems with the quotes turning to double quotes created by my onSave formatter.

--- docs/conf.py
+++ docs/conf.py
@@ -9,38 +9,40 @@ import os
 import sys
 
 # -- Path setup --------------------------------------------------------------
-sys.path.insert(0, os.path.abspath("../src"))
+sys.path.insert(0, os.path.abspath('../src'))
 
 # -- Project information -----------------------------------------------------
-project = "Ubiquity Robotics Documentation"
-copyright = "2025, Ubiquity Robotics"
-author = "Paveljolak"
-version = ""
-release = "1.0.0"
+project = 'Ubiquity Robotics Documentation'
+copyright = '2025, Ubiquity Robotics'
+author = 'Paveljolak'
+version = ''
+release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 extensions = [
-    "sphinx_rtd_theme",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.githubpages",
+    'sphinx_rtd_theme',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
 ]
 
-templates_path = ["_templates"]
-source_suffix = ".rst"
-master_doc = "index"
-language = None
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+language = None 
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 pygments_style = None
 
 # -- HTML output -------------------------------------------------------------
-html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
 
 html_logo = "_static/logos/UbiquityRobotics_Logo_Full_White.png"
 
-html_css_files = ["custom.css"]
+html_css_files = [
+    'custom.css'
+]
 
-html_js_files = ["custom.js"]
+html_js_files = ['custom.js']
 
 # -- RTD lower-left menu setup -----------------------------------------------
 try:
@@ -48,51 +50,51 @@ try:
 except NameError:
     html_context = dict()
 
-html_context["display_lower_left"] = True
+html_context['display_lower_left'] = True
 
 # Repo and environment info
-full_repo = os.environ.get("GITHUB_REPOSITORY", "")
-REPO_NAME = full_repo.split("/")[-1]
-OWNER = os.environ.get("OWNER", "")
+full_repo = os.environ.get('GITHUB_REPOSITORY', '')
+REPO_NAME = full_repo.split('/')[-1] 
+OWNER = os.environ.get('OWNER', '') 
 
 # Use ALL_VERSIONS from workflow environment
 all_versions_env = os.environ.get("ALL_VERSIONS", "")
 ALL_VERSIONS = [v.strip() for v in all_versions_env.splitlines() if v.strip()]
 
-current_version = os.environ.get("CURRENT_BRANCH") or (
-    ALL_VERSIONS[0] if ALL_VERSIONS else "main"
-)
-html_context["current_version"] = current_version
-html_context["version"] = current_version
+current_version = os.environ.get('CURRENT_BRANCH') or (ALL_VERSIONS[0] if ALL_VERSIONS else 'main')
+html_context['current_version'] = current_version
+html_context['version'] = current_version
 
 
 # Base URL for GitHub Pages
 
-# Get the custom domain
+# Get the custom domain 
 CUSTOM_DOMAIN = os.environ.get("CUSTOM_DOMAIN", "")
 
 # If the DOMAIN exists in the workflow then use it
 if CUSTOM_DOMAIN:
     GITHUB_PAGES_BASE = f"https://{REPO_NAME}.{CUSTOM_DOMAIN}"
 else:
-    GITHUB_PAGES_BASE = f"https://{OWNER}.github.io/{REPO_NAME}"  # Set versions for selector with absolute URLs
-
+    GITHUB_PAGES_BASE = f"https://{OWNER}.github.io/{REPO_NAME}"# Set versions for selector with absolute URLs
+    
 # Build the absolute URLs for each version
-html_context["versions"] = [(v, f"{GITHUB_PAGES_BASE}/{v}/") for v in ALL_VERSIONS]
+html_context['versions'] = [
+    (v, f"{GITHUB_PAGES_BASE}/{v}/") for v in ALL_VERSIONS
+]
 
 # project_name = project.replace(' ', '_')
-
 
 # IMPORTANT:
 # ---------------------------------------------------------------------------------------------
 # This part gives error that language does not exists and set it to "None" in the local build process
 # DO NOT CHANGE IT: you will break the version management for the deployed documentation
-html_context["current_language"] = ""
-html_context["language"] = ""
+html_context['current_language'] = ''
+html_context['language'] = ''
 # ---------------------------------------------------------------------------------------------
 
 # -- "Edit on GitHub" links --------------------------------------------------
-html_context["display_github"] = True
-html_context["github_user"] = OWNER
-html_context["github_repo"] = REPO_NAME
-html_context["github_version"] = f"{current_version}/docs/"
+html_context['display_github'] = True
+html_context['github_user'] = OWNER
+html_context['github_repo'] = REPO_NAME
+html_context['github_version'] = f'{current_version}/docs/'
+
